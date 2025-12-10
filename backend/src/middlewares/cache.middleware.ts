5681d2b6ff9476bedc41754d1d972a43bee02148
haldebarhan <theprototype.skartel@gmail.com>
2025-12-10 00:19:49 +0000

    feat: add job crons

--- backend/src/middlewares/cache.middleware.ts
+++ backend/src/middlewares/cache.middleware.ts
@@ -1,30 +1,46 @@
-// cache.middleware.ts
-import { Request, Response, NextFunction } from 'express';
-import redisClient from '../config/redis-client'
+import { Response, NextFunction } from "express";
+import { AuthenticatedRequest } from "../utils/interfaces/authenticated-request";
+import redisClient from "../config/redis-client";
 
-export const cache = async (req: Request, res: Response, next: NextFunction) => {
-    // Utilise l'URL complète comme clé de cache
-    const key = `GET:${req.originalUrl}`;
+export const cache = async (
+  req: AuthenticatedRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  const ttl = 86400;
+  const key = generateCacheKey(req);
 
-    try {
-        const cached = await redisClient.get(key);
-        if (cached) {
-            console.log('[CACHE HIT]', key);
-            return res.json(JSON.parse(cached));
-        }
-
-        console.log('[CACHE MISS]', key);
-
-        // Intercepte la méthode `res.json` pour stocker le résultat dans le cache
-        const originalJson = res.json.bind(res);
-        res.json = (body) => {
-            redisClient.setEx(key, 60, JSON.stringify(body)); // 60 secondes TTL
-            return originalJson(body);
-        };
-
-        return next();
-    } catch (error) {
-        console.error('Cache error:', error);
-        return next();
+  try {
+    const cached = await redisClient.get(key);
+    if (cached) {
+      console.log("[CACHE HIT]", key);
+      return res.json(JSON.parse(cached));
     }
+
+    console.log("[CACHE MISS]", key);
+
+    const originalJson = res.json.bind(res);
+    res.json = (body) => {
+      redisClient.setEx(key, ttl, JSON.stringify(body));
+      return originalJson(body);
+    };
+
+    return next();
+  } catch (error) {
+    console.error("Cache error:", error);
+    return next();
+  }
 };
+
+function generateCacheKey(req: AuthenticatedRequest): string {
+  const url = req.originalUrl.split("?")[0];
+  const params = new URLSearchParams(req.query as any);
+  const sortedParams = Array.from(params.entries()).sort(([a], [b]) =>
+    a.localeCompare(b)
+  );
+
+  const paramsString = new URLSearchParams(sortedParams).toString();
+  const userPart = req.user ? `:user:${req.user.id}` : "";
+
+  return `GET:${url}${paramsString ? "?" + paramsString : ""}${userPart}`;
+}
