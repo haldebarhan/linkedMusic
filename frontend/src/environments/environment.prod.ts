2ecf70539c0bd859418a7bf3d43615d4a24f6740
haldebarhan <theprototype.skartel@gmail.com>
2025-12-13 11:38:32 +0000

    feat: change firebase config for prod

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -3,9 +3,9 @@ export const environment = {
   apiUrl: '/api',
   socketUrl: '',
   firebase: {
-    apiKey: 'AIzaSyBne_6c4t0jXd90lWc2birA6CpkAfEVSiU',
-    authDomain: 'linkedmusic-4d9bb.firebaseapp.com',
-    projectId: 'linkedmusic-4d9bb',
-    appId: '1:928841226151:web:a101dd78cc81def59e8882',
+    apiKey: 'AIzaSyD8yF0NETPT7gsW3B7pecpsXXXw6-6p3VQ',
+    authDomain: 'zikmuzik-80c4f.firebaseapp.com',
+    projectId: 'zikmuzik-80c4f',
+    appId: '1:599596758120:web:225b612f5c4f3a33bf77dd',
   },
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -3,12 +3,14 @@ export const environment = {
   apiUrl: 'http://localhost:8000/api',
   socketUrl: 'http://localhost:8000',
   firebase: {
-    apiKey: 'AIzaSyBne_6c4t0jXd90lWc2birA6CpkAfEVSiU',
-    authDomain: 'linkedmusic-4d9bb.firebaseapp.com',
-    projectId: 'linkedmusic-4d9bb',
-    appId: '1:928841226151:web:a101dd78cc81def59e8882',
+    apiKey: 'AIzaSyD8yF0NETPT7gsW3B7pecpsXXXw6-6p3VQ',
+    authDomain: 'zikmuzik-80c4f.firebaseapp.com',
+    projectId: 'zikmuzik-80c4f',
+    appId: '1:599596758120:web:225b612f5c4f3a33bf77dd',
   },
 };
-// https://alberta-stick-binding-screensaver.trycloudflare.com
-// http://localhost:8000/api
-// socketUrl: 'http://localhost:8000'
+
+// apiKey: 'AIzaSyBne_6c4t0jXd90lWc2birA6CpkAfEVSiU',
+// authDomain: 'linkedmusic-4d9bb.firebaseapp.com',
+// projectId: 'linkedmusic-4d9bb',
+// appId: '1:928841226151:web:a101dd78cc81def59e8882',
